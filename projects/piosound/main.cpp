30381b47afbd227af67a6d6fe15657f663ffdffd
ettore-galli <ettore.galli@gmail.com>
2025-03-15 15:24:47 +0000

    Multipin

--- projects/piosound/main.cpp
+++ projects/piosound/main.cpp
@@ -18,24 +18,19 @@ void pio_sound_program_init(PIO pio, uint sm, uint offset, uint pins[], uint num
     }
 
     pio_sm_set_pindirs_with_mask(pio, sm, mask, mask);
-    // pio_gpio_init(pio, pin);
-    // pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, true);
 
-    // uint32_t mask = (1 << 1) | (1 << 3) | (1 << 7) | (1 << 12);    // Pin 1, 3, 7, 12
-    // uint32_t pindirs = (1 << 1) | (1 << 3) | (1 << 7) | (1 << 12); // Output
-
-    // Configura i pin come output utilizzando le maschere
-    // pio_sm_set_pindirs_with_mask(pio, sm, mask, pindirs;
-
-    // Configura la macchina a stati
     pio_sm_config c = pio_sound_program_get_default_config(offset);
 
     sm_config_set_set_pins(&c, pins[0], number_of_pins); // Imposta i pin di output
 
     pio_sm_init(pio, sm, offset, &c);
 
-    // Avvia la macchina a stati
     pio_sm_set_enabled(pio, sm, true);
+}
+
+uint32_t calculate_delay(float base_step_freq, float freq)
+{
+    return (uint32_t)(base_step_freq / freq) / 2.0;
 }
 
 int main()
@@ -49,8 +44,9 @@ int main()
 
     // Configura il pin 15 come output per il LED
     uint sm = pio_claim_unused_sm(pio, true);
-    uint pins[] = {15, 16, 17, 18};
-    pio_sound_program_init(pio, sm, offset, pins, 4);
+    uint pins[] = {15, 16, 17};
+
+    pio_sound_program_init(pio, sm, offset, pins, 3);
 
     // Imposta la frequenza desiderata (10 Hz)
     float base_step_freq = 100000.0f;                   // Frequenza in Hz
@@ -64,21 +60,21 @@ int main()
     uint32_t delay = 1;
 
     float BASE = 55.0f;
-    float freq = 0.0f;
+    float freq = 4 * BASE;
     uint8_t harmonic = 1;
     const uint8_t NUM_OF_HARMONICS = 15;
 
     while (true)
     {
         sleep_ms(500);
-        harmonic++;
-        if (harmonic > NUM_OF_HARMONICS)
-        {
-            harmonic = 1;
-        }
-        freq = BASE * harmonic;
-        delay = (uint32_t)(base_step_freq / freq) / 2.0;
+        // harmonic++;
+        // if (harmonic > NUM_OF_HARMONICS)
+        // {
+        //     harmonic = 1;
+        // }
+        // freq = BASE * harmonic;
 
+        delay = calculate_delay(base_step_freq, freq);
         pio_sm_put(pio, sm, delay);
     }
 }
